417ffbc218c9911ca966ea6ec9f679188e3b6c0b
Lexej <lexejs@gmail.com>
2021-09-02 12:34:00 +0200

    fmt, print to method

--- gas_sensor_mq135/gas_sensor_mq135.ino
+++ gas_sensor_mq135/gas_sensor_mq135.ino
@@ -1,16 +1,15 @@
-const int mySensValsSize = 6,
-          logEveryLoops = 500,
+const int mySensValsSize = 12,
+          logEveryLoops = 200,
           minRiseLimit = 20,
-          minValueLimit = 220,
-          normValue = 190,
-          ledPin =  LED_BUILTIN,
+          minValueLimit = 200,
+          normValue = 180,
+          ledPin = LED_BUILTIN,
           analogInPin = A0,
-          minHighValueLoops = 10
-                              ;
+          minHighValueLoops = 20;
 
 #include <TM1637.h>
-#include "TM1637.h"
-#define CLK 2//pins definitions for TM1637 and can be changed to other ports
+
+#define CLK 2 //pins definitions for TM1637 and can be changed to other ports
 #define DIO 3
 TM1637 tm1637(CLK, DIO);
 uint8_t Clkpin;
@@ -19,10 +18,10 @@ uint8_t Cmd_SetData;
 uint8_t Cmd_SetAddr;
 uint8_t Cmd_DispCtrl;
 boolean _PointFlag;
-int v1 , v2, v3 = normValue, val , pVal = 0, loops = 0, highValueLoops = 0;
+int v1, v2, v3 = normValue, val, pVal = 0, loops = 0, highValueLoops = 0;
 int mySensVals[mySensValsSize];
 bool isTriggered = false, isBelowNorm = true;
-int ledState = LOW, sensorValue , outputValue = 0, minVal = 150, maxVal = 750;
+int ledState = LOW, sensorValue, outputValue = 0, minVal = normValue, maxVal = normValue * 5;
 // value output to the PWM (analog out)
 
 void setup()
@@ -30,27 +29,30 @@ void setup()
   pinMode(ledPin, OUTPUT);
 
   tm1637.init();
-  tm1637.set(2);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+  tm1637.set(2); //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
 
   Serial.begin(9600);
 
-  for (int count = 0; count < mySensValsSize; count++) {
+  for (int count = 0; count < mySensValsSize; count++)
+  {
     mySensVals[count] = 0;
   }
 }
 
-void loop() {
+void loop()
+{
   loops++;
   sensorValue = analogRead(analogInPin);
 
-  if (sensorValue > maxVal) {
+  if (sensorValue > maxVal)
+  {
     maxVal = sensorValue;
   }
 
-  if (sensorValue < minVal) {
+  if (sensorValue < minVal)
+  {
     minVal = sensorValue;
   }
-
 
   // calc smoothing values and percentage
   v2 = v3;
@@ -58,8 +60,8 @@ void loop() {
   val = v2 + 0.2 * (-v2 + v3);
   //pVal = 100 * ((val - minVal) / (float)(maxVal - minVal));
 
-
-  for (int count = 0; count < mySensValsSize - 1; count++) {
+  for (int count = 0; count < mySensValsSize - 1; count++)
+  {
     mySensVals[count] = mySensVals[count + 1];
   }
   mySensVals[mySensValsSize - 1] = sensorValue;
@@ -67,7 +69,8 @@ void loop() {
   isBelowNorm = true;
 
   v1 = 0;
-  for (int count = 0; count < mySensValsSize - 1; count++) {
+  for (int count = 0; count < mySensValsSize - 1; count++)
+  {
 
     isBelowNorm = isBelowNorm && mySensVals[count] < normValue;
 
@@ -77,39 +80,21 @@ void loop() {
 
   isTriggered = false;
 
-  isTriggered = v1 > minRiseLimit &&  v1 > 0
-                || sensorValue > minValueLimit
-                || val > minValueLimit;
+  isTriggered = v1 > minRiseLimit && v1 > 0 || val > minValueLimit;
 
-  // print the results to the Serial Monitor:
-  if (isTriggered || loops > logEveryLoops) {
+  printSerial();
 
-    if (loops > logEveryLoops) {
-      Serial.print("LOG\t ");
-    } else {
-      Serial.print("!!! TRIGGERED \t ");
-    }
-    Serial.print("sensor = ");   Serial.print(sensorValue);
-    Serial.print("\t Vs = ");    Serial.print(val);
-    //Serial.print("\t Q = ");    Serial.print(pVal);    Serial.print("%");
-
-    //Serial.print("\t  v1 > minRiseLimit = ");    Serial.print( v1 > minRiseLimit);
-    //Serial.print("\t isTriggered = ");    Serial.print(isTriggered);
-    Serial.print("\t up = ");
-    Serial.print(v1);
-
-    Serial.println("");
-    loops = 0;
-  }
-
-  if (  highValueLoops > 0) {
+  if (highValueLoops > 0)
+  {
     highValueLoops++;
   }
-  if (isTriggered) {
+  if (isTriggered)
+  {
     ledState = HIGH;
     highValueLoops++;
   }
-  else if (isBelowNorm && highValueLoops > minHighValueLoops) {
+  else if (isBelowNorm && highValueLoops > minHighValueLoops)
+  {
     ledState = LOW;
     highValueLoops = 0;
   }
@@ -119,20 +104,53 @@ void loop() {
   delay(500);
 }
 
-void display(uint8_t BitAddr, int8_t DispData) {
+void printSerial()
+{
+
+  // print the results to the Serial Monitor:
+  if (isTriggered || loops > logEveryLoops)
+  {
+
+    if (loops > logEveryLoops)
+    {
+      Serial.print("LOG ");
+    }
+    else
+    {
+      Serial.print("!!! TRIGGERED ");
+    }
+    //Serial.print("sensor = ");   Serial.print(sensorValue);
+    Serial.print("\t Vs = ");
+    Serial.print(val);
+    //Serial.print("\t Q = ");    Serial.print(pVal);    Serial.print("%");
+
+    //Serial.print("\t  v1 > minRiseLimit = ");    Serial.print( v1 > minRiseLimit);
+    Serial.print("\t min = ");    Serial.print(minVal);
+    Serial.print("\t up = ");
+    Serial.print(v1);
+
+    Serial.println("");
+    loops = 0;
+  }
+}
+
+void display(uint8_t BitAddr, int8_t DispData)
+{
   tm1637.display(BitAddr, DispData);
 }
 
 void DigitDisplayWrite(int pinClk, int pinDta, int num)
 {
-  if (num > 9999 || num < 0)return;
+  if (num > 9999 || num < 0)
+    return;
 
   static int num_buf = 0;
-  if (num == num_buf)return;
+  if (num == num_buf)
+    return;
   num_buf = num;
 
   // init io
-  Clkpin  = pinClk;
+  Clkpin = pinClk;
   Datapin = pinDta;
   pinMode(Clkpin, OUTPUT);
   pinMode(Datapin, OUTPUT);
